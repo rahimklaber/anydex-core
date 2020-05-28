a3ade5f6e7e48eafecb2b4e6751001dbea952033
Rahim Klabér <r.e.p.klaber@student.tudelft.nl>
2020-05-28 19:24:19 +0200

    Added db table for payments

--- anydex/wallet/stellar/xlm_db.py
+++ anydex/wallet/stellar/xlm_db.py
@@ -1,6 +1,4 @@
-from datetime import datetime
-
-from sqlalchemy import Column, Integer, String, LargeBinary, create_engine, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, create_engine, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -14,9 +12,25 @@ class Secret(Base):
     __tablename__ = 'secrets'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    # public_key = Column(String(32))
     secret = Column(String)
-    address = Column(String)  #
+    address = Column(String)  # address is same as public key
+    transaction_hash = Column(String)  # tx this payment is a part of
+    amount = Column(Integer)
+    asset_type = Column(String)  # we might support more assets
+
+
+class Payment(Base):
+    """
+    Database definition for payments table.
+    In stellar there are multiple types of transactions and payment is one of them.
+    """
+    __tablename__ = 'payments'
+    id = Column(Integer, primary_key=True)
+    payment_id = Column(Integer)
+    succeeded = Column(Boolean)
+    from_ = Column(String)
+    to = Column(String)
+    date_time
 
 
 def initialize_db(db_path):
