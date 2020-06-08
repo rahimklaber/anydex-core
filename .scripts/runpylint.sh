532d5e0a08e380a65f78ad1d5fbe6eee14fa8e77
Calin Georgescu <gcalin224@gmail.com>
2020-06-08 16:35:51 +0200

    Updated linting script to only fail on pylint errors

--- .scripts/runpylint.sh
+++ .scripts/runpylint.sh
@@ -1,4 +1,4 @@
-pylint --fail-under=9 anydex/test/wallet/*.py anydex/wallet/*.py || pylint-exit -rfail -efail $?
+pylint --fail-under=9 anydex/test/wallet/*.py anydex/wallet/*.py || pylint-exit -efail $?
 if [ $? -ne 0 ]; then
   echo "Python returned warning, error or refactor code." >&2
   exit 1
