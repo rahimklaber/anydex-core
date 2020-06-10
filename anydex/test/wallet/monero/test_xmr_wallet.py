98c9f2eb49a9382872498367656cc1e24b726d86
Youri Zwetsloot <yourizwetsloot@yahoo.com>
2020-06-10 20:57:27 +0200

    Fixed Pending tasks warning XMR_Wallet test cases

--- anydex/test/wallet/monero/test_xmr_wallet.py
+++ anydex/test/wallet/monero/test_xmr_wallet.py
@@ -49,6 +49,7 @@ class TestMoneroWallet(AbstractServer):
         self.assertEqual('192.168.178.1', w.host)
         self.assertEqual(18081, w.port)
         self.assertIsNone(w.wallet)
+        w.cancel_all_pending_tasks()
 
     async def test_wallet_connection_alive_fail(self):
         """
@@ -57,6 +58,7 @@ class TestMoneroWallet(AbstractServer):
         w = MoneroWallet()
         fail_backend()
         self.assertFalse(w._wallet_connection_alive())
+        w.cancel_all_pending_tasks()
 
     async def test_wallet_connection_alive_success(self):
         """
@@ -68,6 +70,7 @@ class TestMoneroWallet(AbstractServer):
         self.assertIsNone(result.result())
         self.assertTrue(w.created)
         self.assertTrue(w._wallet_connection_alive())
+        w.cancel_all_pending_tasks()
 
     def test_get_name(self):
         """
@@ -75,6 +78,7 @@ class TestMoneroWallet(AbstractServer):
         """
         w = MoneroWallet()
         self.assertEqual(Cryptocurrency.MONERO.value, w.get_name())
+        w.cancel_all_pending_tasks()
 
     async def test_wallet_creation_fail(self):
         """
@@ -84,6 +88,7 @@ class TestMoneroWallet(AbstractServer):
         fail_backend()
         self.assertAsyncRaises(WalletConnectionError, w.create_wallet())
         self.assertFalse(w.created)
+        w.cancel_all_pending_tasks()
 
     async def test_wallet_creation_success(self):
         """
@@ -94,6 +99,7 @@ class TestMoneroWallet(AbstractServer):
         result = w.create_wallet()
         self.assertIsNone(result.result())
         self.assertTrue(w.created)
+        w.cancel_all_pending_tasks()
 
     async def test_wallet_creation_success_node(self):
         """
@@ -108,6 +114,7 @@ class TestMoneroWallet(AbstractServer):
         self.assertIsNone(result.result())
         self.assertEqual(test_host, w.host)
         self.assertEqual(test_port, w.port)
+        w.cancel_all_pending_tasks()
 
     async def test_get_balance_no_wallet(self):
         """
@@ -120,6 +127,7 @@ class TestMoneroWallet(AbstractServer):
             'currency': 'XMR',
             'precision': 12
         }, await w.get_balance())
+        w.cancel_all_pending_tasks()
 
     async def test_get_balance_wallet(self):
         """
@@ -138,6 +146,7 @@ class TestMoneroWallet(AbstractServer):
             'currency': 'XMR',
             'precision': 12
         }, await w.get_balance())
+        w.cancel_all_pending_tasks()
 
     async def test_transfer_no_wallet(self):
         """
@@ -149,6 +158,7 @@ class TestMoneroWallet(AbstractServer):
                                   priority=1,
                                   unlock_time=0)
         self.assertIsNone(result.result())
+        w.cancel_all_pending_tasks()
 
     async def test_transfer_wallet_enough(self):
         """
@@ -174,6 +184,7 @@ class TestMoneroWallet(AbstractServer):
                                   priority=1,
                                   unlock_time=0)
         self.assertEqual(TEST_HASH, result.result())
+        w.cancel_all_pending_tasks()
 
     async def test_transfer_wallet_not_enough(self):
         """
@@ -193,6 +204,7 @@ class TestMoneroWallet(AbstractServer):
                                                              payment_id='test_id',
                                                              priority=1,
                                                              unlock_time=0))
+        w.cancel_all_pending_tasks()
 
     async def test_transfer_multiple_no_wallet(self):
         """
@@ -206,6 +218,7 @@ class TestMoneroWallet(AbstractServer):
         ]
 
         self.assertAsyncRaises(WalletConnectionError, w.transfer_multiple(transfers, priority=3))
+        w.cancel_all_pending_tasks()
 
     async def test_transfer_multiple_wallet(self):
         """
@@ -229,6 +242,7 @@ class TestMoneroWallet(AbstractServer):
 
         hashes = await w.transfer_multiple(transfers, priority=3)
         self.assertEqual(2, len(hashes.result()))
+        w.cancel_all_pending_tasks()
 
     async def test_transfer_multiple_wallet_not_enough(self):
         """
@@ -247,6 +261,7 @@ class TestMoneroWallet(AbstractServer):
         w.wallet = mock_wallet
 
         self.assertAsyncRaises(InsufficientFunds, await w.transfer_multiple(transfers, priority=3))
+        w.cancel_all_pending_tasks()
 
     def test_get_address_no_wallet(self):
         """
@@ -255,6 +270,7 @@ class TestMoneroWallet(AbstractServer):
         w = MoneroWallet()
         addr = w.get_address()
         self.assertEqual('', addr)
+        w.cancel_all_pending_tasks()
 
     def test_get_address_wallet(self):
         """
@@ -267,6 +283,7 @@ class TestMoneroWallet(AbstractServer):
         w.wallet = mock_wallet
         addr = w.get_address()
         self.assertEqual(TEST_ADDRESS, addr)
+        w.cancel_all_pending_tasks()
 
     def test_get_transactions_no_wallet(self):
         """
@@ -274,6 +291,7 @@ class TestMoneroWallet(AbstractServer):
         """
         w = MoneroWallet()
         self.assertAsyncRaises(WalletConnectionError, w.get_transactions())
+        w.cancel_all_pending_tasks()
 
     async def test_get_transactions_wallet(self):
         """
@@ -324,6 +342,7 @@ class TestMoneroWallet(AbstractServer):
             'timestamp': ts,
             'description': 'Confirmations: 17'
         }, transactions.result()[1])
+        w.cancel_all_pending_tasks()
 
     async def test_get_payments(self):
         """
@@ -349,6 +368,7 @@ class TestMoneroWallet(AbstractServer):
 
         payments = await w._get_payments()
         self.assertEqual(2, len(payments))
+        w.cancel_all_pending_tasks()
 
     def test_normalize_transaction_incoming_payment(self):
         """
@@ -382,6 +402,7 @@ class TestMoneroWallet(AbstractServer):
             'timestamp': ts,
             'description': 'Confirmations: 12'
         }, w._normalize_transaction(payment))
+        w.cancel_all_pending_tasks()
 
     def test_normalize_transaction_outgoing_payment(self):
         """
@@ -414,6 +435,7 @@ class TestMoneroWallet(AbstractServer):
             'timestamp': ts,
             'description': 'Confirmations: 23'
         }, w._normalize_transaction(payment))
+        w.cancel_all_pending_tasks()
 
     def test_get_incoming_payments_no_wallet(self):
         """
@@ -421,6 +443,7 @@ class TestMoneroWallet(AbstractServer):
         """
         w = MoneroWallet()
         self.assertAsyncRaises(WalletConnectionError, w.get_incoming_payments())
+        w.cancel_all_pending_tasks()
 
     async def test_get_incoming_payments_wallet(self):
         """
@@ -437,6 +460,7 @@ class TestMoneroWallet(AbstractServer):
 
         self.assertIsNotNone(result)
         self.assertTrue(isinstance(result[0], Payment))
+        w.cancel_all_pending_tasks()
 
     def test_get_outgoing_payments_no_wallet(self):
         """
@@ -444,6 +468,7 @@ class TestMoneroWallet(AbstractServer):
         """
         w = MoneroWallet()
         self.assertAsyncRaises(WalletConnectionError, w.get_outgoing_payments())
+        w.cancel_all_pending_tasks()
 
     async def test_get_outgoing_payments_wallet(self):
         """
@@ -460,6 +485,7 @@ class TestMoneroWallet(AbstractServer):
 
         self.assertIsNotNone(result)
         self.assertTrue(isinstance(result[0], Payment))
+        w.cancel_all_pending_tasks()
 
     def test_min_unit(self):
         """
@@ -467,6 +493,7 @@ class TestMoneroWallet(AbstractServer):
         """
         w = MoneroWallet()
         self.assertEqual(1, w.min_unit())  # 1 piconero
+        w.cancel_all_pending_tasks()
 
     def test_precision(self):
         """
@@ -474,6 +501,7 @@ class TestMoneroWallet(AbstractServer):
         """
         w = MoneroWallet()
         self.assertEqual(12, w.precision())
+        w.cancel_all_pending_tasks()
 
     def test_get_identifier(self):
         """
@@ -481,6 +509,7 @@ class TestMoneroWallet(AbstractServer):
         """
         w = MoneroWallet()
         self.assertEqual('XMR', w.get_identifier())
+        w.cancel_all_pending_tasks()
 
     def test_get_confirmations_no_wallet(self):
         """
@@ -490,6 +519,7 @@ class TestMoneroWallet(AbstractServer):
         self.assertIsNone(w.wallet)
         p = Payment()
         self.assertAsyncRaises(WalletConnectionError, w.get_confirmations(p))
+        w.cancel_all_pending_tasks()
 
     async def test_get_confirmations_wallet(self):
         """
@@ -508,6 +538,7 @@ class TestMoneroWallet(AbstractServer):
 
         p = Payment()
         self.assertEqual(4, await w.get_confirmations(p))
+        w.cancel_all_pending_tasks()
 
 
 class TestTestnetMoneroWallet(AbstractServer):
@@ -519,6 +550,7 @@ class TestTestnetMoneroWallet(AbstractServer):
         w = MoneroTestnetWallet()
         self.assertTrue(w.TESTNET)
         self.assertEqual('tribler_testnet', w.wallet_name)
+        w.cancel_all_pending_tasks()
 
     def test_get_name(self):
         """
@@ -526,6 +558,7 @@ class TestTestnetMoneroWallet(AbstractServer):
         """
         w = MoneroTestnetWallet()
         self.assertEqual('Testnet XMR', w.get_name())
+        w.cancel_all_pending_tasks()
 
     def test_get_identifier(self):
         """
@@ -533,3 +566,4 @@ class TestTestnetMoneroWallet(AbstractServer):
         """
         w = MoneroTestnetWallet()
         self.assertEqual('TXMR', w.get_identifier())
+        w.cancel_all_pending_tasks()
