e738933731eef5881fd33996c9553b2c3f23d0d4
Rahim Klabér <r.e.p.klaber@student.tudelft.nl>
2020-06-16 21:20:31 +0200

    Added from_dicts method

--- anydex/wallet/ethereum/token/token_wallet.py
+++ anydex/wallet/ethereum/token/token_wallet.py
@@ -1,35 +1,55 @@
-# from typing import TypedDict
-
-from ipv8.util import fail, succeed
-
-from anydex.wallet.ethereum.eth_wallet import EthereumWallet, EthereumTestnetWallet
-from anydex.wallet.wallet import Wallet, InsufficientFunds
-from web3 import Web3
-import pathlib
 import json
+import pathlib
+
+from web3 import Web3
 
 from anydex.wallet.ethereum.eth_db import Transaction
+from anydex.wallet.ethereum.eth_wallet import EthereumTestnetWallet
 from anydex.wallet.ethereum.token.token_provider import TokenProvider
+from anydex.wallet.wallet import InsufficientFunds
 
 
 class Erc20TokenWallet(EthereumTestnetWallet):
 
-    def __init__(self, contract_address, identifier, name, decimals, abi, provider: TokenProvider, db_folder):
+    def __init__(self, contract_address, identifier, name, decimals, provider: TokenProvider, db_folder):
+        abi = self.abi_from_json()
         self.identifier = identifier
         self.name = name
         self.decimals = decimals
         self.contract_address = contract_address
         self.contract = Web3().eth.contract(Web3.toChecksumAddress(contract_address), abi=abi)
-        self.provider = provider if provider else TokenProvider(contract_address)
+        self.provider = provider if provider else TokenProvider(contract_address, abi)
         super().__init__(db_folder, self.provider)
 
     @staticmethod
     def abi_from_json(path_to_file=None):
+        """
+        Read the abi from the json file
+        :param path_to_file: path to abi json file
+
+        :return: abi
+        """
         if not path_to_file:
             path_to_file = pathlib.Path.joinpath(pathlib.Path(__file__).parent.absolute(), 'abi.json')
         with open(path_to_file) as abi_file:
             abi_json = json.loads(abi_file.read())
         return abi_json
+
+    @classmethod
+    def from_dicts(cls, tokens, db_folder):
+        """
+        Create a list of new wallets from the given dicts.
+
+        :param db_folder: folder enclosing the database file
+        :param tokens: a list of dictionaries that contains the token info
+        :return: list of created wallets
+        """
+        wallets = []
+        if type(tokens) == dict:  # if it's only one dict and not a list
+            return [cls.from_dict(tokens, db_folder)]
+        for token in tokens:
+            wallets.append(cls.from_dict(token, db_folder))
+        return wallets
 
     @classmethod
     def from_dict(cls, token, db_folder):
@@ -44,11 +64,11 @@ class Erc20TokenWallet(EthereumTestnetWallet):
 
 
         :param db_folder: folder enclosing the database file
-        :param token: a dictionary that contains the info defined in the TokenDict class
+        :param token: a dictionary that contains the token info
         :return: a new instance of this class
         """
         abi = cls.abi_from_json()
-        return cls(token['contract_address'], token['identifier'], token['name'], token['precision'], abi, None,
+        return cls(token['contract_address'], token['identifier'], token['name'], token['precision'], None,
                    db_folder)
 
     def get_identifier(self):
